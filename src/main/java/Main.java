49fa1543a964e45bc68ac053647c7916abcde2a7
Misenooooo <dolnakmichal@gmail.com>
2016-04-13 18:54:45 +0200

    Skuska 2 classy vytvorene

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by michal on 13.4.2016.
+ */
+public class Main {
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ src/main/java/Map.java
@@ -0,0 +1,5 @@
+/**
+ * Created by michal on 13.4.2016.
+ */
+public class Map {
+}
